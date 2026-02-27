d67a1e2b80065df3526f04a2533ca63d4bc772a1
Thomas Ruiz <thomasrruiz03@gmail.com>
2026-02-26 21:19:28 -0700

    Creating Popup for projects
    To make viewing projects more easily, creating a popup triggered by learn more button that gives a more in depth view of selected project

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+
+
+var popup = document.getElementById("popUp");
+var modal = document.getElementById("myModal");
+var exit = document.getElementById("closePopup");
+//var span = document.getElementsByClassName("close")[0];
+
+function openPopup() {
+    popup.classList.add("open-popup");
+}
+
+function closePopup() {
+        popup.classList.remove("open-popup");
+}
+
+
+
